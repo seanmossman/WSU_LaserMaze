34a8edd788d2f4254b81362cecd06e35687d1e8f
Sean Mossman <sean.mossman@wsu.edu>
2017-03-18 14:06:38 -0700

    Updated file directory
    Some old versions have been removed as they are now stored in history
    (and other locations). The necessary file directorys were updated in
    run_maze.py as well.

--- OSA_LaserMaze_MomsWeekend/AllOff/AllOff.ino
+++ OSA_LaserMaze_MomsWeekend/AllOff/AllOff.ino
@@ -1,8 +1,8 @@
-  void setup() {
+void setup() {
   // put your setup code here, to run once:
   for (int i = 0; i < 13; i++){
     pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
+    digitalWrite(i, LOW);
   }
 }
 
